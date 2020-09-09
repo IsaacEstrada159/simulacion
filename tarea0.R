ecf116717c16407e4c63fe911177c3e4b1f79c48
IsaacEstrada159 <70659682+IsaacEstrada159@users.noreply.github.com>
2020-09-08 23:07:20 -0500

    Create tarea0.R

---
+++ tarea0.R
@@ -0,0 +1,15 @@
+doble <-function(x) {
+     return (2*x);
+     }
+
+A <- 99999
+B <- 1111
+
+if (doble(A) > B){
+    cat("hola mundo\n");
+}
+for( x in 0:19) {
+    cat(2**x, "\n");
+}
+lista= c(1,2,3,4,6,7,8,11,14,15,16,18,22,21);
+cat(lista, "\n");
