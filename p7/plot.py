3c8edecf96edffa54bbcf70d43c0f893923fa034
IsaacEstrada159 <70659682+IsaacEstrada159@users.noreply.github.com>
2021-10-11 19:36:45 -0500

    lol

--- p7/plot.py
+++ p7/plot.py
@@ -10,12 +10,15 @@ step = 0.25
 x = np.arange(low, high, step)
 y = np.arange(low, high, step)
 x, y = np.meshgrid(x, y)
-z = ((x + 0.7)**4 - 12 * x**2 - 30 * x + (y + 0.7)**4 - 15 * y**2 - 30 * y)/300
+#z = ((x + 0.7)**4 - 12 * x**2 - 30 * x + (y + 0.7)**4 - 15 * y**2 - 30 * y)/300
+z = ((x + 0.7)**4 - 50 * x**2 - 50 * x + (y + 0.7)**4 - 50 * y**2 + 50 * y)/300
  
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-s = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+s = ax.plot_surface(x, y, z, cmap=cm.viridis, linewidth=0, antialiased=False)
 ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter('%.01f'))
-fig.colorbar(s, shrink=0.5, aspect=5)
+plt.xlabel('x')
+plt.ylabel('y')
+fig.colorbar(s, shrink=0.5, aspect=6)
 plt.savefig("p7p_2d.png")
